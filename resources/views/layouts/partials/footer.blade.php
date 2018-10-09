f602f4e22d7a242b972880226513fc8f531338de
Stanislav <stas_garcia@mail.ru>
2018-10-09 21:13:40 +0300

    Minor CSS update

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <div class="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
-        <p class="navbar-text pull-left">
+        <p class="navbar-text pull-left small">
             © 2016-{{date('Y')}} - Site Built By <a  href="https://github.com/StanislavBogatov" target="_blank">Stanislav Bogatov</a>
 
         </p>
 
-        <a class="navbar-btn btn btn-social btn-github pull-right" href="https://github.com/StanislavBogatov/BugWall_Visual_Bugtracker" target="_blank">
+        <a class="navbar-btn btn btn-sm btn-social btn-github pull-right" href="https://github.com/StanislavBogatov/Visual_Bugtracker" target="_blank">
             <span class="fa fa-github"></span> Check out on GitHub
         </a>
     </div>
--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/addon-styles.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/navbar.css') }}" />
 
     <link rel="stylesheet" href="{{ asset('bootstrap-social/assets/css/font-awesome.css') }}" />
