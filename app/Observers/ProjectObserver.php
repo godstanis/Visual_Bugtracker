76aab247562187059cc4213ba565b7c3ec46364f
Stanislav <stas_garcia@mail.ru>
2018-10-13 12:43:52 +0300

    PHPDoc blocks for ProjectObserver updated with additional info

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -9,14 +9,14 @@ class ProjectObserver
 {
 
     /**
-     * Handle the Project "created" event.
+     * Handle the Project "created" event and create
+     * according project access for creator.
      *
      * @param  Project  $project
      * @return void
      */
     public function created(Project $project)
     {
-
         $project_access = app()->make(ProjectAccess::class);
         $project_access->user_id = $project->creator_user_id;
         $project_access->project_id = $project->id;
@@ -24,18 +24,17 @@ class ProjectObserver
         $project_access->save();
 
         \Log::info('Project with id:'.$project->id.'created by user with id:'.$project->creator_user_id);
-
     }
 
     /**
-     * Listen to the Project deleting event.
+     * Listen to the Project deleting event
+     * and delete all binded models.
      *
      * @param  Project  $project
      * @return void
      */
     public function deleting(Project $project)
     {
-
         $project->project_access()->delete();
         $project->issues()->delete();
         $project->boards()->delete();
