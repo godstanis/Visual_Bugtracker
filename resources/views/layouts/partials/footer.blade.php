86462d25b7de5508361ab05ad20f74db1523f69a
Stanislav <stas_garcia@mail.ru>
2018-10-15 18:31:17 +0300

    Footer updated to sticky behviour

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,21 +1,20 @@
-<div class="navbar navbar-default navbar-fixed-bottom">
+<div id="footer" class="footer navbar navbar-default navbar-fixed-bottom">
     <div class="container">
         <p class="navbar-text pull-left small">
-            © 2016-{{date('Y')}} - Site Built By <a  href="https://github.com/stasgar" target="_blank">Stanislav Bogatov</a>
-
+            <span>
+                © 2016-{{date('Y')}} - Site Built By <a  href="https://github.com/stasgar" target="_blank">Stanislav Bogatov &nbsp;</a>
+            </span>
         </p>
+
         <a class="navbar-btn btn btn-sm btn-social btn-github" href="https://github.com/stasgar/Visual_Bugtracker" target="_blank">
-            <span class="fa fa-github"></span> Check out on GitHub
+            <span class="fa fa-github"></span><span class="btn-github-text">Check out on GitHub</span>
         </a>
 
-
-        <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown pull-right">
-
-            <a href="#" class="dropdown-toggle language-dropdown-toggle" data-toggle="dropdown">
+        <div class="dropdown pull-right navbar-btn">
+            <button class="dropdown-toggle language-dropdown-toggle btn btn-default" data-toggle="dropdown">
                 <span class="caret"></span>
                 <img width="18px" src="{{ asset('images/language_icons/'.App::getLocale().'.png') }}" alt="{{ Config::get('languages')[App::getLocale()] }}">
-            </a>
+            </button>
             <ul class="dropdown-menu">
                 @foreach (Config::get('languages') as $lang => $language)
                     @if ($lang != App::getLocale())
@@ -28,9 +27,6 @@
                     @endif
                 @endforeach
             </ul>
-        </li>
-
-
-        </ul>
+        </div>
     </div>
 </div>
