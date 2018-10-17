34ce5b0ba5c14328752081eec8687f0e464f44bf
Stanislav <stas_garcia@mail.ru>
2018-10-17 16:08:07 +0300

    Path resource created

---
+++ app/Http/Resources/PathResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PathResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        self::withoutWrapping(); // {"data":[...]} wrapping remove for this response.
+
+        $path = [
+            'path_slug'=>$this->path_slug,
+            'stroke'=>$this->stroke_color,
+            'stroke-width'=>$this->stroke_width,
+            'd'=>$this->path_data
+        ];
+
+        return $path;
+    }
+}
