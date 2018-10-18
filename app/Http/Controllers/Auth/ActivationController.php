468f6b09c2135619bcdd0b8e18fb8e79b5a1d290
Stanislav <stas_garcia@mail.ru>
2018-10-18 13:28:33 +0300

    UserRepository removed

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -6,28 +6,21 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\UserActivation;
-use App\Repositories\UserRepository;
 use App\Repositories\UserActivationRepository;
 
 class ActivationController extends Controller
 {
-
-    protected $user_repository;
     protected $user_activation_repository;
 
-    public function __construct(UserRepository $user_repository, UserActivationRepository $user_activation_repository)
+    public function __construct(UserActivationRepository $user_activation_repository)
     {
-        $this->user_repository = $user_repository;
         $this->user_activation_repository = $user_activation_repository;
     }
 
     public function activate($token)
     {
-
         $activated = $this->user_activation_repository->activateUserByToken($token);
-
-        if($activated)
-        {
+        if($activated) {
             return view('auth.activation.success');
         }
 
