9931f1a366e5c041ced270ae75d430b70b803725
Stanislav <stas_garcia@mail.ru>
2018-10-18 19:29:43 +0300

    Unused repositories deleted (BoardRepository, PathRepository)

--- app/Http/Controllers/Bugtracker/PathController.php
+++ app/Http/Controllers/Bugtracker/PathController.php
@@ -4,22 +4,14 @@ namespace App\Http\Controllers\Bugtracker;
 
 use App\Http\Controllers\Controller;
 
-use App\Repositories\PathRepository;
 use Illuminate\Http\Request;
 
 use App\Project;
 use App\Board;
-use App\Path;
 
 class PathController extends Controller
 {
 
-    protected $path_repository;
-
-    public function __construct(PathRepository $repository)
-    {
-        $this->path_repository = $repository;
-    }
 
     public function savePath(Request $request, Project $project, Board $board)
     {
@@ -37,7 +29,7 @@ class PathController extends Controller
 
     public function deletePath(Request $request, Project $project, Board $board)
     {
-        $path = $board->paths()->where(['path_slug'=>$request->path_slug])->first();
+        $path = $board->paths()->where('path_slug', $request->path_slug)->first();
 
         broadcast( new \App\Events\Pusher\PathDeleted($path) )->toOthers();
 
