134d095b83117ab7c93c83850b92f117f334d0c1
stasgar <stasgarcia@gmail.com>
2018-11-04 15:18:36 +0300

    Fixes 419 timeout error in testing environment

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,15 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    public function handle($request, \Closure $next)
+    {
+        // We will enable csrf verification only for non-testing requests.
+        if ('testing' !== env('APP_ENV'))
+        {
+            return parent::handle($request, $next);
+        }
+
+        return $next($request);
+    }
 }
